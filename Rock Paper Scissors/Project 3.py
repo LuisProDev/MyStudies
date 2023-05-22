01cbd04bc6594da54429ea1b57f35314ade55fd5
Luis Gustavo <emailparacontato1000@gmail.com>
2023-05-22 17:19:19 -0300

    P3 Changes

--- Rock Paper Scissors/Project 3.py
+++ Rock Paper Scissors/Project 3.py
@@ -31,22 +31,24 @@ user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 f
 all_choices = [rock, paper, scissors]
 computer_choice = random.randint(0, 2)
 
-print(all_choices[user_choice])
-
-print("Computer chose: \n")
-print(all_choices[computer_choice])
-
 if user_choice >= 3 or user_choice < 0:
     print("Invalid number!")
-elif computer_choice == 0 and user_choice == 2:
-    print("You lose!")
-elif computer_choice < user_choice:
-    print("You win!")
-elif user_choice == 0 and computer_choice == 2:
-    print("You win!")
-elif user_choice < computer_choice:
-    print("You lose!")
-elif user_choice == computer_choice:
-    print("It's a draw!")
+else:
+    print(all_choices[user_choice])
+
+    print("Computer chose: \n")
+    print(all_choices[computer_choice])
+
+
+    if computer_choice == 0 and user_choice == 2:
+        print("You lose!")
+    elif computer_choice < user_choice:
+        print("You win!")
+    elif user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif user_choice < computer_choice:
+        print("You lose!")
+    elif user_choice == computer_choice:
+        print("It's a draw!")
 
     
