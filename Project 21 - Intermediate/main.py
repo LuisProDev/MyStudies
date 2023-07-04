a891a820053f1365eb45205e99f9a59a1cc3f284
LuisProDev <emailparacontato1000@gmail.com>
2023-07-04 11:45:49 -0300

    .

--- Project 21 - Intermediate/main.py
+++ Project 21 - Intermediate/main.py
@@ -1,2 +1,19 @@
+import turtle
 import pandas
 
+my_screen = turtle.Screen()
+my_screen.title("U.S States Game")
+image = "blank_states_img.gif"
+my_screen.addshape(image)
+turtle.shape(image)
+
+# while True:
+guess = turtle.textinput("Guess", "Guess a State")
+states = pandas.read_csv("50_states.csv")
+guess_row = states[states.state == guess]
+x_location = guess_row.x
+y_location = guess_row.y
+print(x_location)
+
+
+my_screen.mainloop()
