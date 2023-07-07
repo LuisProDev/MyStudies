92b44c3135c6a16f0112621917e9dea77649ef80
Luis Gustavo <emailparacontato1000@gmail.com>
2023-07-07 14:52:13 -0300

    project 22 done, really simple

--- Project 22 - Intermediate/main.py
+++ Project 22 - Intermediate/main.py
@@ -1,24 +1,4 @@
-# student_dict = {
-#     "student": ["Angela", "James", "Lily"],
-#     "score": [56, 76, 98]
-# }
-#
-# #Looping through dictionaries:
-# for (key, value) in student_dict.items():
-#     #Access key and value
-#     pass
-#
 import pandas
-# student_data_frame = pandas.DataFrame(student_dict)
-#
-# #Loop through rows of a data frame
-# for (index, row) in student_data_frame.iterrows():
-#     #Access index and row
-#     #Access row.student or row.score
-#     pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
@@ -28,3 +8,6 @@ nato_dict = {row['letter']: row['code'] for _, row in df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
+user_input = list(input("Enter a word: ").upper())
+coded_list = [nato_dict[x] for x in user_input]
+print(coded_list)
